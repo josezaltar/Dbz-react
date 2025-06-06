e3988c39b234407974bafd3173754d7653f50bce
zezaltar <josezaltar@gmail.com>
2025-06-06 13:52:03 -0300

    Bora

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,12 +5,12 @@ const Footer: React.FC = () => {
   return (
     <Container>
       <Text>
+        <p>DBZ Revolution &copy; 2025 - Desenvolvido por Jose Zaltar.</p>
         <p>
-          Dragon Ball Z Revolution &copy; 2025 - Desenvolvido por Jose Zaltar.
-        </p>
-        <p>
-          Dragon Ball Z e todos os personagens relacionados são marcas
-          registradas de Akira Toriyama
+          Este site é um projeto de fã. Dragon Ball, Dragon Ball Z, Dragon Ball
+          GT e Dragon Ball Super são propriedades de Akira Toriyama, Toei
+          Animation, Shueisha e Funimation. Todos os direitos reservados aos
+          seus respectivos criadores.
         </p>
       </Text>
     </Container>
