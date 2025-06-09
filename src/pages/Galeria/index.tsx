3509902427e980a669bdce66fb549ce73e9d11b9
zezaltar <josezaltar@gmail.com>
2025-06-08 21:59:20 -0300

    Adicionando imagens

--- src/pages/Galeria/index.tsx
+++ src/pages/Galeria/index.tsx
@@ -1,7 +1,35 @@
-import React from "react";
-import { Container, Title, SubTitle, Text, VideoContainer } from "./styles"; // Importa os estilos
+import React, { useState } from "react";
+import {
+  Container,
+  Title,
+  SubTitle,
+  Text,
+  VideoContainer,
+  Modal,
+  ModalContent,
+  CloseButton,
+} from "./styles"; // Importa os estilos
+
+// Importação das imagens
+import foto2 from "../../assets/images/prints/Captura de tela 2025-06-08 213236.png";
+import foto3 from "../../assets/images/prints/Captura de tela 2025-06-08 214136.png";
+import foto4 from "../../assets/images/prints/Captura de tela 2025-06-08 214144.png";
+import foto5 from "../../assets/images/prints/Captura de tela 2025-06-08 214153.png";
 
 const Galeria: React.FC = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [modalImage, setModalImage] = useState<string | null>(null);
+
+  const openModal = (image: string) => {
+    setModalImage(image);
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+    setModalImage(null);
+  };
+
   return (
     <Container>
       <Title>Galeria de Mídia</Title>
@@ -9,6 +37,34 @@ const Galeria: React.FC = () => {
         Explore a galeria de vídeos e outros conteúdos relacionados ao universo
         de DBZ Revolution! Veja trailers, teasers e momentos épicos do jogo.
       </Text>
+
+      <SubTitle>Imagens do Jogo</SubTitle>
+      <div>
+        <img
+          src={foto3} // Corrigido para usar a variável de importação
+          alt="Imagem 2"
+          onClick={() => openModal(foto3)} // Passando a variável correta
+          style={{ width: "200px", margin: "10px", cursor: "pointer" }}
+        />
+        <img
+          src={foto4} // Corrigido para usar a variável de importação
+          alt="Imagem 3"
+          onClick={() => openModal(foto4)} // Passando a variável correta
+          style={{ width: "200px", margin: "10px", cursor: "pointer" }}
+        />
+        <img
+          src={foto5} // Corrigido para usar a variável de importação
+          alt="Imagem 4"
+          onClick={() => openModal(foto5)} // Passando a variável correta
+          style={{ width: "200px", margin: "10px", cursor: "pointer" }}
+        />
+        <img
+          src={foto2} // Corrigido para usar a variável de importação
+          alt="Imagem 5"
+          onClick={() => openModal(foto2)} // Passando a variável correta
+          style={{ width: "200px", margin: "10px", cursor: "pointer" }}
+        />
+      </div>
 
       <SubTitle>Vídeo Oficial</SubTitle>
       <VideoContainer>
@@ -23,6 +79,19 @@ const Galeria: React.FC = () => {
           allowFullScreen
         ></iframe>
       </VideoContainer>
+
+      {isModalOpen && modalImage && (
+        <Modal>
+          <ModalContent>
+            <img
+              src={modalImage}
+              alt="Imagem em tamanho maior"
+              style={{ maxWidth: "100%", maxHeight: "80vh" }}
+            />
+            <CloseButton onClick={closeModal}>X</CloseButton>
+          </ModalContent>
+        </Modal>
+      )}
     </Container>
   );
 };
--- src/pages/Galeria/styles.ts
+++ src/pages/Galeria/styles.ts
@@ -1,32 +1,33 @@
-// src/pages/Galeria/styles.ts
 import styled from "styled-components";
 import { Colors, Spacing, Breakpoints, Shadows } from "../../styles/variables"; // Importando as variáveis
 
 export const Container = styled.section`
   padding: ${Spacing.large} ${Spacing.small};
   text-align: center;
-  background-color: ${Colors.background}; /* Usando a cor de fundo definida em variables.ts */
-  color: ${Colors.secondary}; /* Usando a cor de texto definida em variables.ts */
+  background-color: ${Colors.background};
+  color: ${Colors.secondary};
   border-radius: 10px;
   max-width: 95%;
   margin: ${Spacing.large} auto;
   overflow: hidden;
-  box-shadow: ${Shadows.heavy}; /* Usando sombra média definida em variables.ts */
+  box-shadow: ${Shadows.heavy};
 
-  /* Responsividade */
   @media (max-width: ${Breakpoints.tablet}) {
     padding: ${Spacing.medium} ${Spacing.small};
+  }
+
+  @media (max-width: ${Breakpoints.mobile}) {
+    padding: ${Spacing.small};
   }
 `;
 
 export const Title = styled.h1`
   font-size: 3rem;
-  color: ${Colors.primary}; /* Usando a cor primária definida em variables.ts */
+  color: ${Colors.primary};
   margin-bottom: ${Spacing.large};
   text-align: center;
   -webkit-text-stroke: 0.8px #000;
 
-  /* Responsividade */
   @media (max-width: ${Breakpoints.tablet}) {
     font-size: 2.5rem;
   }
@@ -39,25 +40,34 @@ export const Title = styled.h1`
 export const SubTitle = styled.h2`
   -webkit-text-stroke: 0.5px #000;
   font-size: 2rem;
-  color: ${Colors.primary}; /* Usando a cor primária definida em variables.ts */
+  color: ${Colors.primary};
   margin-bottom: ${Spacing.medium};
   text-align: center;
+
+  @media (max-width: ${Breakpoints.mobile}) {
+    font-size: 1.8rem;
+  }
 `;
 
 export const Text = styled.p`
   font-size: 1.25rem;
   margin-top: ${Spacing.small};
-  color: ${Colors.secondary}; /* Usando a cor de texto definida em variables.ts */
+  color: ${Colors.secondary};
   text-align: center;
   margin-bottom: ${Spacing.xLarge};
 
   strong {
-    color: ${Colors.primary}; /* Destaca palavras fortes em laranja */
+    color: ${Colors.primary};
   }
 
-  /* Responsividade */
   @media (max-width: ${Breakpoints.tablet}) {
     font-size: 1.1rem;
+  }
+
+  @media (max-width: ${Breakpoints.mobile}) {
+    font-size: 1rem;
+    margin-top: ${Spacing.medium};
+    margin-bottom: ${Spacing.large};
   }
 `;
 
@@ -69,6 +79,110 @@ export const VideoContainer = styled.div`
 
   iframe {
     border-radius: 10px;
-    box-shadow: ${Shadows.medium}; /* Usando sombra média definida em variables.ts */
+    box-shadow: ${Shadows.medium};
+  }
+
+  @media (max-width: ${Breakpoints.mobile}) {
+    iframe {
+      width: 100%;
+      height: auto;
+    }
+  }
+`;
+
+export const ImageGallery = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: ${Spacing.small};
+  margin-top: ${Spacing.medium};
+
+  @media (max-width: ${Breakpoints.tablet}) {
+    gap: ${Spacing.xsmall};
+  }
+
+  @media (max-width: ${Breakpoints.mobile}) {
+    gap: ${Spacing.small};
+  }
+`;
+
+export const GalleryImage = styled.img`
+  width: 200px;
+  height: 150px;
+  object-fit: cover;
+  cursor: pointer;
+  border-radius: 10px;
+  box-shadow: ${Shadows.medium};
+  transition: transform 0.3s ease;
+
+  &:hover {
+    transform: scale(
+      1.05
+    ); // Efetua um efeito de zoom quando o usuário passar o mouse
+  }
+
+  @media (max-width: ${Breakpoints.tablet}) {
+    width: 180px;
+    height: 135px;
+  }
+
+  @media (max-width: ${Breakpoints.mobile}) {
+    width: 160px;
+    height: 120px;
+  }
+`;
+
+export const Modal = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.8);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+`;
+
+export const ModalContent = styled.div`
+  position: relative;
+  background: #000;
+  border-radius: 10px;
+  max-width: 100%;
+  max-height: 100%;
+
+  @media (max-width: ${Breakpoints.tablet}) {
+    max-width: 95%;
+  }
+
+  @media (max-width: ${Breakpoints.mobile}) {
+    max-width: 100%;
+  }
+`;
+
+export const ModalImage = styled.img`
+  width: 100%;
+  height: 100%;
+  max-width: 100%;
+  max-height: 100%;
+`;
+
+export const CloseButton = styled.button`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background-color: transparent;
+  border: none;
+  font-size: 2rem;
+  color: ${Colors.primary};
+  cursor: pointer;
+
+  &:hover {
+    color: ${Colors.secondary};
+  }
+
+  @media (max-width: ${Breakpoints.mobile}) {
+    font-size: 1.5rem;
   }
 `;
